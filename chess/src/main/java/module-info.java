87ec0ed7d3a9986e4442d182ef1fdbbbc0235f83
João Piedade <153988325+JoaoPiedade07@users.noreply.github.com>
2026-02-05 18:00:27 +0000

    Erros corrigidos

--- chess/src/main/java/module-info.java
+++ chess/src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module chess {
+    requires java.base;
     requires javafx.fxml;
     requires javafx.controls;
+    requires transitive javafx.graphics;
     opens ru.job4j.chess to javafx.fxml;
     exports ru.job4j.chess;
+    exports ru.job4j.chess.firuges;
 }
--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,9 +1,7 @@
 package ru.job4j.chess;
-
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
-import ru.job4j.chess.firuges.black.BishopBlack;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -19,6 +17,5 @@ public class LogicTest {
             logic.move(Cell.C1, Cell.H6);
         });
         assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
-                ru.job4j.chess.firuges.Cell
     }
 }
