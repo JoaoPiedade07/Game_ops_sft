8c2cf6c8b43ca83c7e59644bb32caccffbc996ae
Marcelo Ramos <marcelorcramos@gmail.com>
2026-02-05 19:05:04 +0000

    Rename firuges to figures and update build/tests
    Rename package 'ru.job4j.chess.firuges' to 'ru.job4j.chess.figures' and update imports across all figure classes and usages. Add a message constructor to OccupiedCellException. Update Maven settings: set compiler source/target to 25 in root pom and chess module, add compiler properties in chess/pom.xml, remove -Dglass.platform option, and remove includeTestSourceDirectory from checkstyle config. Add/adjust tests: introduce SimpleTest, modify LogicTest (add testCellEnum and adjust test signature), and keep backup/save copies of previous LogicTest. Small formatting/schema tweaks in pom files.

---
+++ chess/src/test/java/ru/job4j/chess/SimpleTest.java
@@ -0,0 +1,11 @@
+package ru.job4j.chess;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+    @Test
+    public void simpleTest() {
+        assertTrue(true);
+    }
+}
